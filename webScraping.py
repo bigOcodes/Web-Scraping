472ac310b100ab787b21840d9ccb8e3d837b08cb
Pravakar <paltasingh10@gmail.com>
2022-10-10 16:18:52 +0530

    commit 1

---
+++ webScraping.py
@@ -0,0 +1,28 @@
+# web scrapping using beautifulsoup
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+total_c = 0
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs,'\n')
+    
+    total_c += int(tag.contents[0])
+    
+print("Total Comments:",total_c)
